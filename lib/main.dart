c5ea78c287607f30dc2c3b81dcd2de072823dec3
ernesto alfonso <ernesto.caballero@datys.cu>
2019-01-31 12:09:57 -0500

    added 1/2

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: "Papyrus pocket",),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -31,6 +32,7 @@ class MyHomePageState extends State<MyHomePage> {
 
   static final List<MaterialColor> bgColors = <MaterialColor>[
     Colors.blue,
+    Colors.grey,
     Colors.purple,
     Colors.orange,
     Colors.lime,
@@ -41,6 +43,7 @@ class MyHomePageState extends State<MyHomePage> {
   ];
   static final List<Color> textColors = <Color>[
     Colors.white,
+    Colors.black,
     Colors.white,
     Colors.black,
     Colors.black,
@@ -56,15 +59,15 @@ class MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new PageView.builder(
         itemBuilder: (context, i) {
-          int posicion = i.abs() % 7;
+          int posicion = i.abs() % 8;
 
-          TextStyle textStyle = new TextStyle(fontSize: 400.0, color: textColors[posicion]);
+          TextStyle textStyle = new TextStyle(fontSize: MediaQuery.of(context).size.width/1.5, color: textColors[posicion]);
 
           return Material(
             type: MaterialType.transparency,
             textStyle: textStyle,
             child: new Container(
-                padding: EdgeInsets.all(24.0),
+                padding: EdgeInsets.all(14.0),
                 color: bgColors[posicion],
                 child: widgetList(textStyle, posicion)
             ),
@@ -76,6 +79,9 @@ class MyHomePageState extends State<MyHomePage> {
 
 
   Widget widgetList(textStyle, position){
+    double height = MediaQuery.of(context).size.height * 0.25;
+    double widht = MediaQuery.of(context).size.width * 0.27;
+
     print(position);
       switch(position){
         case 0: return Stack(
@@ -85,7 +91,7 @@ class MyHomePageState extends State<MyHomePage> {
             ),
             Align(
               alignment: Alignment.bottomLeft,
-              child: Image.asset("images/cake.png", height: 220.0, width: 215.0,),
+              child: Image.asset("images/cake.png", height: height, width: widht,),
             )
           ],
         );
@@ -93,68 +99,80 @@ class MyHomePageState extends State<MyHomePage> {
           fit: StackFit.expand,
           children: <Widget>[
             Center(
-              child: Text("1"),
+              child: Text("1/2", style: textStyle.copyWith(fontSize: MediaQuery.of(context).size.width/2.1),),
             ),
             Align(
-              alignment: Alignment.topRight,
-              child: Image.asset("images/easy.png", height: 220.0, width: 215.0,),
+              alignment: Alignment.topLeft,
+              child: Image.asset("images/firework.png", height: height * 1.5, width: widht * 1.5,),
             )
           ],
         );
         case 2: return Stack(
+          fit: StackFit.expand,
           children: <Widget>[
             Center(
-              child: Text("2"),
+              child: Text("1"),
             ),
             Align(
-              alignment: Alignment.bottomRight,
-              child: Image.asset("images/work.png", height: 220.0, width: 215.0,),
+              alignment: Alignment.topRight,
+              child: Image.asset("images/easy.png", height: height * 1.5, width: widht * 1.5,),
             )
           ],
         );
         case 3: return Stack(
           children: <Widget>[
             Center(
-              child: Text("3"),
+              child: Text("2"),
             ),
             Align(
-              alignment: Alignment.topLeft,
-              child: Image.asset("images/hard.png", height: 220.0, width: 215.0,),
+              alignment: Alignment.bottomRight,
+              child: Image.asset("images/work.png", height: height * 1.2, width: widht * 1.2,),
             )
           ],
         );
         case 4: return Stack(
           children: <Widget>[
             Center(
-              child: Text("5+"),
+              child: Text("3"),
             ),
             Align(
-              alignment: Alignment.bottomCenter,
-              child: Image.asset("images/noway.png", height: 220.0, width: 215.0,),
+              alignment: Alignment.topLeft,
+              child: Image.asset("images/hard.png", height: height * 1.3, width: widht * 1.3,),
             )
           ],
         );
         case 5: return Stack(
           children: <Widget>[
             Center(
-              child: Text("%"),
+              child: Text("5+"),
             ),
             Align(
-              alignment: Alignment.topLeft,
-              child: Image.asset("images/split.png", height: 220.0, width: 215.0,),
+              alignment: Alignment.bottomCenter,
+              child: Image.asset("images/noway.png", height: height * 1.08, width: widht * 1.08,),
             )
           ],
         );
         case 6: return Stack(
           children: <Widget>[
+            Center(
+              child: Text("%"),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Image.asset("images/split.png", height: height - 15.5, width: widht - 15.5,),
+            )
+          ],
+        );
+        case 7: return Stack(
+          children: <Widget>[
             Align(
               alignment: Alignment.topCenter,
               child: Text(
-                "Pasteles!!", style: textStyle.copyWith(fontSize: 80.0),),
+                "Pasteles", style: textStyle.copyWith(fontSize: 80.0),),
             ),
             Center(
               child: Image.asset(
-                "images/merienda.png", height: 320.0, width: 315.0,),
+                "images/merienda.png", height: height * 2, width: widht * 2,),
             )
           ],
         );
